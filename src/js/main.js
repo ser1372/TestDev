735523ab499468bf84fa32de13100d10277bed53
Sergey <87072531+ser1372@users.noreply.github.com>
2022-09-15 15:37:24 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,24 @@
 /* smooth scoll */
 
 $('a[href*="#"]')
+
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
+  
     // On-page links
           if (
           location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
           && 
           location.hostname == this.hostname
         ) {
+            
+            
       // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
+    
 
           if (target.length) 
           {
